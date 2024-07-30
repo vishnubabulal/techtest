e386209c07b56649afddf667d610f899272fdbed
Vishnu B Lal <157651416+vishnubabulal@users.noreply.github.com>
2024-07-30 03:18:10 +0200

    Add files via upload

---
+++ resources/views/articles.blade.php
@@ -0,0 +1,15 @@
+<!-- resources/views/articles.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Articles</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <div id="app">
+        <h1>Articles</h1>
+        <ul id="article-list"></ul>
+    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
