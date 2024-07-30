9bfe2cef51d13dd463cdae73c94fdd45e32f7d44
Vishnu B Lal <157651416+vishnubabulal@users.noreply.github.com>
2024-07-30 03:18:43 +0200

    Add files via upload

---
+++ storage/framework/views/c8c4f0ef06e435ee38ed84e471f60766.php
@@ -0,0 +1,16 @@
+<!-- resources/views/articles.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Articles</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo e(asset('css/app.css')); ?>">
+</head>
+<body>
+    <div id="app">
+        <h1>Articles</h1>
+        <ul id="article-list"></ul>
+    </div>
+    <script src="<?php echo e(asset('js/app.js')); ?>"></script>
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\techtest\TechTest\resources\views/articles.blade.php ENDPATH**/ ?>
